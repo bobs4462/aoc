87125b36fbd575b42782edd784f8d73f78bd84c6
Makhmudov Babur <bobs4462@gmail.com>
2021-03-08 12:28:57 +0300

    Solved day 2 part 2

--- src/solver/y2015/d2.rs
+++ src/solver/y2015/d2.rs
@@ -33,30 +33,11 @@ impl Solver for D2 {
         Ok(())
     }
     fn solve_part_one(&self, data: Vec<u8>) -> String {
-        const OFST: u8 = 0x30;
         let mut total: usize = 0;
         for line in data.lines() {
-            let mut dims = [0; 3];
             let ln = line.expect("Coudn't read a line in data set");
-            for (i, num) in ln.as_bytes().split(|&c| c == b'x').enumerate() {
-                let mut n: usize = 0;
-                let mut rank: isize = (num.len() - 1) as isize;
-                const BASE: usize = 0xA;
-                for &b in num {
-                    n += ((b - OFST) as usize) * BASE.pow(rank as u32);
-                    rank -= 1;
-                }
-                dims[i] = n;
-            }
-            let bt = dims[0] * dims[1] * 2;
-            let lr = dims[1] * dims[2] * 2;
-            let fb = dims[0] * dims[2] * 2;
-            dims[0] = bt / 2;
-            dims[1] = lr / 2;
-            dims[2] = fb / 2;
-            let box_paper = bt + lr + fb;
-            let extra_paper = dims.iter().min().expect("Couldn't find minimum value");
-            total += box_paper + *extra_paper;
+            let [a, b, c] = self.dims(ln);
+            total += (a * b + b * c + a * c) * 2 + (a * b).min(a * c).min(b * c)
         }
         format!(
             "The total amount of wrapping paper is:\n{} square feet",
@@ -64,26 +45,33 @@ impl Solver for D2 {
         )
     }
     fn solve_part_two(&self, data: Vec<u8>) -> String {
-        const LPAREN: u8 = 0x28;
-        const RPAREN: u8 = 0x29;
-
-        let mut position = 0;
-        let mut below_ground = 0;
-        for (i, &b) in data.iter().enumerate() {
-            match b {
-                LPAREN => position += 1,
-                RPAREN => position -= 1,
-                _ => break,
-            }
-            if position == -1 {
-                below_ground = i + 1;
-                break;
-            }
+        let mut total: usize = 0;
+        for line in data.lines() {
+            let ln = line.expect("Coudn't read a line in data set");
+            let mut dims = self.dims(ln);
+            dims.sort();
+            let [a, b, c] = dims;
+            total += (a + b) * 2 + a * b * c;
         }
-        format!(
-            "THE INSTRUCTION NUMBER TO GET SANTA BELOW GROUND IS:\n{}",
-            below_ground
-        )
+        format!("The total length of ribbon is:\n{} feet", total)
+    }
+}
+
+impl D2 {
+    fn dims(&self, ln: String) -> [usize; 3] {
+        const OFST: u8 = 0x30;
+        let mut dims = [0; 3];
+        for (i, num) in ln.as_bytes().split(|&c| c == b'x').enumerate() {
+            let mut n: usize = 0;
+            let mut rank: isize = (num.len() - 1) as isize;
+            const BASE: usize = 0xA;
+            for &b in num {
+                n += ((b - OFST) as usize) * BASE.pow(rank as u32);
+                rank -= 1;
+            }
+            dims[i] = n;
+        }
+        dims
     }
 }
 
@@ -105,12 +93,11 @@ mod tests {
     #[test]
     fn test_part_two() {
         use crate::solver::Solver;
-        let test_data = String::from("()()()))").into_bytes();
+        const DATA: &'static str = r#"2x3x4
+1x1x10
+"#;
         let solver = super::D2 {};
-        let res = solver.solve_part_two(test_data);
-        assert_eq!(
-            res,
-            String::from("THE INSTRUCTION NUMBER TO GET SANTA BELOW GROUND IS:\n7")
-        );
+        let res = solver.solve_part_two(DATA.as_bytes().to_vec());
+        assert_eq!(res, String::from("The total length of ribbon is:\n48 feet"));
     }
 }
--- src/statics/mod.rs
+++ src/statics/mod.rs
@@ -1,9 +1,12 @@
 use crate::challenge::{Day, Year};
-use crate::solver::y2015::d1::D1;
+use crate::solver::y2015::{d1::D1, d2::D2};
 
 lazy_static! {
     pub static ref YEARS: Vec<Year> = vec![
-        Year::new(2015, vec![Day::new(1, Box::new(D1))]),
+        Year::new(
+            2015,
+            vec![Day::new(1, Box::new(D1)), Day::new(2, Box::new(D2)),]
+        ),
         Year::new(2016, vec![Day::new(1, Box::new(D1))]),
         Year::new(2017, vec![Day::new(1, Box::new(D1))])
     ];
