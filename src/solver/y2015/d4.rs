0556822dfcf58e8bc995588c0fc87e76996f5d62
Makhmudov Babur <bobs4462@gmail.com>
2021-03-13 12:45:53 +0300

    Micro optimization of md5 function

--- src/solver/y2015/d4.rs
+++ src/solver/y2015/d4.rs
@@ -1,9 +1,25 @@
 /// --- Day 4: The Ideal Stocking Stuffer ---
-// use crate::solver::{Solution, Solver};
-//
-// pub(crate) struct D4;
-//
+use crate::solver::{Solution, Solver};
 
+pub(crate) struct D4;
+
+impl Solver for D4 {
+    fn validate(&self, input: &[u8]) -> Result<(), String> {
+        drop(input);
+        Ok(())
+    }
+    fn solve_part_one(&self, data: Vec<u8>) -> Solution {
+        md5(data);
+        Solution::new("", String::new())
+    }
+
+    fn solve_part_two(&self, data: Vec<u8>) -> Solution {
+        md5(data);
+        Solution::new("", String::new())
+    }
+}
+
+#[allow(dead_code)]
 fn digest(array: [u32; 4]) -> String {
     let bytes: &[u8] =
         unsafe { std::slice::from_raw_parts((&array) as *const u32 as *const u8, 16) };
@@ -34,6 +50,13 @@ static K: [u32; CHUNK] = [
     0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
     0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391,
 ];
+static PADDING: [u8; CHUNK] = [
+    0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+];
+
 fn md5(mut data: Vec<u8>) -> [u32; 4] {
     let mut a0: u32 = 0x67452301; // A
     let mut b0: u32 = 0xefcdab89; // B
@@ -42,8 +65,8 @@ fn md5(mut data: Vec<u8>) -> [u32; 4] {
 
     // original data length in bits
     let original_len = (data.len() * 8) as u64;
-    // pad the data with bit 1, and because we cannot operate on bits, add another 7 0s
-    data.push(0b10000000);
+    // pad the data with 1, without any checks
+    data.push(PADDING[0]);
     // Find the length of data in bytes modulo 64, find the difference with 56 = 448 bits, the
     // difference can be negative or positive, if it's positive modulo magic will leave it as is,
     // but if it's negative it will become a positive number 64 - abs(difference), which is the
@@ -52,7 +75,7 @@ fn md5(mut data: Vec<u8>) -> [u32; 4] {
         (((56 - (data.len() % CHUNK) as isize) + CHUNK as isize) % CHUNK as isize) as usize;
     // append the padding amount of bytes so that the total length of data becomes congruent to 56
     // modulo 64
-    data.append(&mut vec![0; padding]);
+    data.extend(PADDING[1..=padding].iter());
     // append 8 bytes which encode little endian representation of original length of data
     data.append(&mut original_len.to_le_bytes().to_vec());
 
