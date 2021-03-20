78c609d463e8548cff48587004a4d4f6da6b46a8
Makhmudov Babur <bobs4462@gmail.com>
2021-03-21 01:42:12 +0300

    Solved 2015 day 9 part 2

--- src/solver/y2015/d9.rs
+++ src/solver/y2015/d9.rs
@@ -42,87 +42,95 @@ impl Solver for D9 {
     }
 
     fn solve_part_one(&self, data: Vec<u8>) -> Solution {
-        let lines = data.split(|&c| c == b'\n');
-        let mut nodes: HashMap<&str, RefNode> = HashMap::new();
-        for l in lines {
-            let path = unsafe { std::str::from_utf8_unchecked(l) };
-            let path: Vec<&str> = path.split(' ').collect();
-
-            let weigth = path[4].parse().unwrap();
-            let source: RefNode;
-            let destination: RefNode;
-            if let Some(src) = nodes.get(path[0]) {
-                source = Rc::clone(src);
-                if let Some(dst) = nodes.get(path[2]) {
-                    destination = Rc::clone(dst);
-                } else {
-                    destination = Rc::new(RefCell::new(Node {
-                        name: path[2],
-                        paths: Vec::new(),
-                    }));
-                }
-            } else {
-                if let Some(dst) = nodes.get(path[2]) {
-                    destination = Rc::clone(dst);
-                } else {
-                    destination = Rc::new(RefCell::new(Node {
-                        name: path[2],
-                        paths: Vec::new(),
-                    }));
-                }
-                source = Rc::new(RefCell::new(Node {
-                    name: path[0],
-                    paths: Vec::new(),
-                }));
-            }
-            source.borrow_mut().paths.push(Path {
-                dst: Rc::clone(&destination),
-                weigth,
-            });
-            destination.borrow_mut().paths.push(Path {
-                dst: Rc::clone(&source),
-                weigth,
-            });
-            nodes.insert(path[0], source);
-            nodes.insert(path[2], destination);
-        }
-        let count = nodes.len();
-        let mut shortest = usize::MAX;
-        for (_, v) in nodes {
-            let mut tovisit = Vec::new();
-            tovisit.push(Runner {
-                current: Rc::clone(&v),
-                total: 0,
-                visited: HashSet::new(),
-            });
-            while !tovisit.is_empty() {
-                let mut r = tovisit.pop().unwrap();
-                r.visited.insert(r.current.borrow().name);
-
-                if r.visited.len() == count {
-                    shortest = shortest.min(r.total);
-                    continue;
-                }
-
-                for p in r.current.borrow().paths.iter() {
-                    if r.visited.contains(p.dst.borrow().name) {
-                        continue;
-                    }
-                    tovisit.push(Runner {
-                        current: Rc::clone(&p.dst),
-                        total: r.total + p.weigth,
-                        visited: r.visited.clone(),
-                    });
-                }
-            }
-        }
+        let shortest = walk(data, usize::min, usize::MAX);
         Solution::new("Shortest path is:", shortest.to_string())
     }
 
     fn solve_part_two(&self, data: Vec<u8>) -> Solution {
-        drop(data);
-        Solution::new("", "".to_string())
+        let longest = walk(data, usize::max, usize::MIN);
+        Solution::new("Longest path is:", longest.to_string())
     }
+}
+
+fn walk<F>(data: Vec<u8>, cmp: F, start: usize) -> usize
+where
+    F: Fn(usize, usize) -> usize,
+{
+    let lines = data.split(|&c| c == b'\n');
+    let mut nodes: HashMap<&str, RefNode> = HashMap::new();
+    for l in lines {
+        let path = unsafe { std::str::from_utf8_unchecked(l) };
+        let path: Vec<&str> = path.split(' ').collect();
+
+        let weigth = path[4].parse().unwrap();
+        let source: RefNode;
+        let destination: RefNode;
+        if let Some(src) = nodes.get(path[0]) {
+            source = Rc::clone(src);
+            if let Some(dst) = nodes.get(path[2]) {
+                destination = Rc::clone(dst);
+            } else {
+                destination = Rc::new(RefCell::new(Node {
+                    name: path[2],
+                    paths: Vec::new(),
+                }));
+            }
+        } else {
+            if let Some(dst) = nodes.get(path[2]) {
+                destination = Rc::clone(dst);
+            } else {
+                destination = Rc::new(RefCell::new(Node {
+                    name: path[2],
+                    paths: Vec::new(),
+                }));
+            }
+            source = Rc::new(RefCell::new(Node {
+                name: path[0],
+                paths: Vec::new(),
+            }));
+        }
+        source.borrow_mut().paths.push(Path {
+            dst: Rc::clone(&destination),
+            weigth,
+        });
+        destination.borrow_mut().paths.push(Path {
+            dst: Rc::clone(&source),
+            weigth,
+        });
+        nodes.insert(path[0], source);
+        nodes.insert(path[2], destination);
+    }
+    let count = nodes.len();
+    let mut val = start;
+    for (_, v) in nodes {
+        let mut tovisit = Vec::new();
+        tovisit.push(Runner {
+            current: Rc::clone(&v),
+            total: 0,
+            visited: HashSet::new(),
+        });
+        while !tovisit.is_empty() {
+            let mut r = tovisit.pop().unwrap();
+            r.visited.insert(r.current.borrow().name);
+
+            if r.visited.len() == count {
+                val = cmp(val, r.total);
+                continue;
+            }
+
+            for p in r.current.borrow().paths.iter() {
+                if r.visited.contains(p.dst.borrow().name) {
+                    continue;
+                }
+                tovisit.push(Runner {
+                    current: Rc::clone(&p.dst),
+                    total: r.total + p.weigth,
+                    visited: r.visited.clone(),
+                });
+            }
+        }
+    }
+    val
 }
 
 #[cfg(test)]
@@ -167,5 +175,45 @@ Tambi to Straylight = 70"#
             .to_vec();
         let res = solver.solve_part_one(data);
         assert_eq!(res.value, "207");
+    }
+    #[test]
+    fn test_part_two() {
+        let solver = super::D9;
+        let data =
+            b"London to Dublin = 464\nLondon to Belfast = 518\nDublin to Belfast = 141".to_vec();
+        let res = solver.solve_part_two(data);
+        assert_eq!(res.value, "982");
+        let data = r#"Faerun to Norrath = 129
+Faerun to Tristram = 58
+Faerun to AlphaCentauri = 13
+Faerun to Arbre = 24
+Faerun to Snowdin = 60
+Faerun to Tambi = 71
+Faerun to Straylight = 67
+Norrath to Tristram = 142
+Norrath to AlphaCentauri = 15
+Norrath to Arbre = 135
+Norrath to Snowdin = 75
+Norrath to Tambi = 82
+Norrath to Straylight = 54
+Tristram to AlphaCentauri = 118
+Tristram to Arbre = 122
+Tristram to Snowdin = 103
+Tristram to Tambi = 49
+Tristram to Straylight = 97
+AlphaCentauri to Arbre = 116
+AlphaCentauri to Snowdin = 12
+AlphaCentauri to Tambi = 18
+AlphaCentauri to Straylight = 91
+Arbre to Snowdin = 129
+Arbre to Tambi = 53
+Arbre to Straylight = 40
+Snowdin to Tambi = 15
+Snowdin to Straylight = 99
+Tambi to Straylight = 70"#
+            .as_bytes()
+            .to_vec();
+        let res = solver.solve_part_two(data);
+        assert_eq!(res.value, "804");
     }
 }
