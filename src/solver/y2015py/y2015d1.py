fed7bb26b67b3a07a42346d9516c6cd71251d235
inordon <inordon@yandex.ru>
2021-03-21 14:14:38 +0300

    and here we go

--- src/solver/y2015py/y2015d1.py
+++ src/solver/y2015py/y2015d1.py
@@ -1,5 +1,7 @@
 from pathlib import Path
-filepath = Path("inputs/y2015d1.txt")
+filepath = Path("/Users/dinislam/PycharmProjects/aoc/inputs/y2015d1.txt")
+
+print(filepath.name)
 
 with open(filepath, 'r') as input_file:
     for line in input_file:
