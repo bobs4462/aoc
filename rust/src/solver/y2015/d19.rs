6bde7be08f1ae2e16a1d8df3ba9cf592f519393c
Makhmudov Babur <bobs4462@gmail.com>
2021-05-05 19:24:14 +0300

    Struggling with 2015 day 19

--- rust/src/solver/y2015/d19.rs
+++ rust/src/solver/y2015/d19.rs
@@ -1,7 +1,7 @@
 /// --- Day 19: Medicine for Rudolph ---
 pub struct D19;
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use crate::solver::{Solution, Solver};
 
@@ -36,40 +36,38 @@ impl Solver for D19 {
 }
 
 struct Reactor {
-    transorms: HashMap<Vec<u8>, Vec<u8>>,
+    transorms: Vec<(Vec<u8>, Vec<u8>)>,
     molecule: Vec<u8>,
+    transmutations: HashSet<String>,
 }
 
 impl Reactor {
     fn new(input: Vec<u8>) -> Self {
-        let mut transorms = HashMap::new();
+        let mut transorms = Vec::with_capacity(64);
         let mut lines = input.split(|&c| c == b'\n');
         while let Some(line) = lines.next() {
             if line.is_empty() {
                 break;
             }
             let mut transorm = line.split(|&c| c == b' ');
-            transorms.insert(
+            transorms.push((
                 transorm.next().unwrap().to_vec(),
                 transorm.nth(1).unwrap().to_vec(),
-            );
+            ));
         }
         let molecule = lines.next().unwrap().to_vec();
         Reactor {
             transorms,
             molecule,
+            transmutations: HashSet::with_capacity(1024),
         }
     }
 
     fn calibrate(&self) -> usize {
-        let mut sequence = self.molecule.iter().peekable();
-        while let Some(v) = sequence.next() {
-            let mut source = vec![v];
-            if let Some(&n) = sequence.peek() {
-                if n.is_ascii_lowercase() {}
-                source.push(n);
-                sequence.next();
-            }
+        for t in &self.transorms {
+            let mut sequence = self.molecule.iter().enumerate();
+
+            while let Some((i, l)) = sequence.next() {}
         }
         0
     }
