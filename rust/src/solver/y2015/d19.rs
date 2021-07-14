004c3054b1fcd652ddaaf758dba0c5aea38ab69e
Makhmudov Babur <bobs4462@gmail.com>
2021-07-14 22:08:14 +0300

    Added parser and string traverse function

--- rust/src/solver/y2015/d19.rs
+++ rust/src/solver/y2015/d19.rs
@@ -1,7 +1,7 @@
 /// --- Day 19: Medicine for Rudolph ---
 pub struct D19;
 
-use std::collections::{HashMap, HashSet};
+use std::io::Read;
 
 use crate::solver::{Solution, Solver};
 
@@ -35,40 +35,111 @@ impl Solver for D19 {
     }
 }
 
+fn shash<T: Iterator<Item = u8>>(string: T) -> usize {
+    let mut res = 0;
+    let mut len = 0;
+
+    for (i, b) in string.enumerate() {
+        res += b as usize ^ i + b as usize * i;
+        len = i + 1;
+    }
+
+    res + len
+}
+
 struct Reactor {
-    transorms: Vec<(Vec<u8>, Vec<u8>)>,
-    molecule: Vec<u8>,
-    transmutations: HashSet<String>,
+    original: Vec<u8>,
+    transforms: Vec<(Vec<u8>, Vec<u8>)>,
+    hashes: Vec<usize>,
 }
 
 impl Reactor {
-    fn new(input: Vec<u8>) -> Self {
-        let mut transorms = Vec::with_capacity(64);
-        let mut lines = input.split(|&c| c == b'\n');
-        while let Some(line) = lines.next() {
-            if line.is_empty() {
+    fn new(setup: &[u8]) -> Self {
+        let lines = setup.split(|&b| b == b'\n');
+
+        let mut transforms = Vec::with_capacity(64);
+
+        let original = lines.rev().next().unwrap().to_owned();
+
+        let lines = setup.split(|&b| b == b'\n');
+
+        for l in lines {
+            if l.is_empty() {
                 break;
             }
-            let mut transorm = line.split(|&c| c == b' ');
-            transorms.push((
-                transorm.next().unwrap().to_vec(),
-                transorm.nth(1).unwrap().to_vec(),
-            ));
+            let mut source = Vec::new();
+            let mut dest = Vec::new();
+
+            let mut l = l.iter();
+
+            while let Some(b) = l.next() {
+                if b.is_ascii_whitespace() {
+                    break;
+                }
+                source.push(*b);
+            }
+            while let Some(b) = l.next() {
+                if b.is_ascii_whitespace() {
+                    break;
+                }
+            }
+            while let Some(b) = l.next() {
+                dest.push(*b);
+            }
+            transforms.push((source, dest));
         }
-        let molecule = lines.next().unwrap().to_vec();
+
         Reactor {
-            transorms,
-            molecule,
-            transmutations: HashSet::with_capacity(1024),
+            original,
+            transforms,
+            hashes: Vec::new(),
         }
     }
+    fn ignite(&mut self) -> usize {
+        for (s, d) in &self.transforms {
+            let mut i = 0;
 
-    fn calibrate(&self) -> usize {
-        for t in &self.transorms {
-            let mut sequence = self.molecule.iter().enumerate();
-
-            while let Some((i, l)) = sequence.next() {}
+            for (j, b) in self.original.iter().enumerate() {
+                if b != unsafe { s.get_unchecked(i) } {
+                    i = 0;
+                    continue;
+                }
+                if i == s.len() - 1 {
+                    let chain = self.original[0..i - s.len() + 1]
+                        .chain(&d[..])
+                        .chain(&self.original[j + 1..])
+                        .bytes()
+                        .flatten();
+                    self.hashes.push(shash(chain));
+                } else {
+                    i += 1;
+                }
+            }
         }
         0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn reactor_new() {
+        use super::Reactor;
+        let r = Reactor::new(
+            r#"H => HO
+H => OH
+O => HH
+
+HOH"#
+                .to_owned()
+                .as_bytes(),
+        );
+        assert_eq!(r.original, b"HOH");
+        assert_eq!(r.transforms[0].0, vec![b'H']);
+        assert_eq!(r.transforms[0].1, vec![b'H', b'O']);
+        assert_eq!(r.transforms[1].0, vec![b'H']);
+        assert_eq!(r.transforms[1].1, vec![b'O', b'H']);
+        assert_eq!(r.transforms[2].0, vec![b'O']);
+        assert_eq!(r.transforms[2].1, vec![b'H', b'H']);
     }
 }
